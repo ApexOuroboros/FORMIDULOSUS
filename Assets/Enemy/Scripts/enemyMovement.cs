dd7bf6233ec471b2da95e3d56f0d76f4ffac5393
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2022-12-29 18:56:17 +0000

    enemy movement pt4
    basic movement. very buggy. quite scary

--- Assets/Enemy/Scripts/enemyMovement.cs
+++ Assets/Enemy/Scripts/enemyMovement.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyMovement : MonoBehaviour
 {
-    public GameObject player; //what the enemy will follow
-
-    public float speed;
+    public Transform Player;
+    int Speed = 4;
+    int MinDist = 1;
 
 
     // Update is called once per frame
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player"); //find the player
+       
     }
 
     //move towards the player when not rendered
     //enemy moves at set speed
-    private void OnBecomeInvisible()
+    void Update()
     {
-        transform.position = player.transform.position - player.transform.forward;
-        //add something else, enemy isnt moving
-    }
+        transform.LookAt(Player);
 
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+    }
 }
