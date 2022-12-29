a6adf850af4f5bfce0c53433533e341bea10549d
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2022-12-29 19:30:59 +0000

    on invisable
    trying to move when not rendered. why debug no work

--- Assets/Enemy/Scripts/enemyMovement.cs
+++ Assets/Enemy/Scripts/enemyMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,26 +8,40 @@ using UnityEngine.UI;
 public class enemyMovement : MonoBehaviour
 {
     public Transform Player;
-    int Speed = 4;
-    int MinDist = 1;
+    int speed = 1; //scary fast if speed 4
+    int minDist = 1;
 
 
     // Update is called once per frame
     void Start()
     {
-       
+        OnBecomeVisible();
     }
 
     //move towards the player when not rendered
     //enemy moves at set speed
     void Update()
     {
+       
+    }
+
+    void OnBecomeVisible()
+    {
+        enabled = false;
+        Debug.Log("not moving");
+    }
+
+    void OnBecomeInvisible()
+    {
+        Debug.Log("moving");
+
         transform.LookAt(Player);
 
-        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        if (Vector3.Distance(transform.position, Player.position) >= minDist)
         {
 
-            transform.position += transform.forward * Speed * Time.deltaTime;
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
     }
+
 }
